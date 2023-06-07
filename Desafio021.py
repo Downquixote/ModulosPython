bd37ea0eca336146c8cddfbea8a0c4de968a5331
Lucas Brandão <122632969+Downquixote@users.noreply.github.com>
2023-06-07 10:17:26 -0300

    Desafio021.py

--- Desafio021.py
+++ Desafio021.py
@@ -1,12 +1,8 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
 import pygame
-# Para iniciar o uso do pygame, utilize:
 pygame.init()
-# Para usar o mixer, utilize:
-pygame.mixer.music.load('santo.mp3') # Não esqueça de adicionar a música a pasta.
-# Para iniciar a música, utilize:
+pygame.mixer.music.load('santo.mp3')
 pygame.mixer.music.play()
-# Para terminar o programa quando a música terminar, utilize:
 input()
 pygame.event.wait()
 
